ec4f2b40849f362c3c97b932d441edbe034b6551
kirill13079 <kirillzhenkevich13@gmail.com>
2022-11-03 13:46:56 +0300

    task 1

--- console-app/console-app.cpp
+++ console-app/console-app.cpp
@@ -1,6 +1,64 @@
 ﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    enum menus
+    {
+        MENU_1 = 1,
+        ABOUT = 2,
+        EXIT = 3,
+        DEFAULT
+    };
+
+    menus currentMenu = DEFAULT;
+
+    int selectedMenu;
+
+    do
+    {
+        system("cls");
+
+        if (currentMenu != DEFAULT) 
+        {
+            cout << "\tТекущее меню: " << currentMenu << "\n";
+
+            if (currentMenu == ABOUT)
+            {
+                cout << "\n\tАвтор программы Иванов И.И.\n\n";
+            }
+        }
+        else 
+        {
+            cout << "Меню\n";
+            cout << "Пожалуйста сделайте свой выбор\n";
+        }
+
+        cout << "1 - Меню 1\n";
+        cout << "2 - Об авторе\n";
+        cout << "3 - Выход\n";
+        cout << "Выбор: ";
+        cin >> selectedMenu;
+
+        switch (selectedMenu)
+        {
+        case MENU_1:
+            currentMenu = MENU_1;
+            break;
+        case ABOUT:
+            currentMenu = ABOUT;
+            break;
+        case EXIT:
+            currentMenu = EXIT;
+            break;
+        default:
+            currentMenu = DEFAULT;
+            break;
+        }
+    } while (currentMenu != EXIT);
 }
